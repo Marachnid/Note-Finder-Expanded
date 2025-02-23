5305fda247da4e7d4f3e7b0dd3e7a6c2cfc0d997
Marachnid <mcherry2@madisoncollege.edu>
2025-02-22 23:07:22 -0600

    Tomcat/MVC configuration up and running with logs

--- src/main/java/note/finder/controller/modifyScalesRouter.java
+++ src/main/java/note/finder/controller/modifyScalesRouter.java
@@ -1,14 +1,35 @@
 package note.finder.controller;
 
-
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.io.IOException;
+import java.time.LocalDateTime;
 
+/**
+ * Routing servlet - only forwards user to scale management interface
+ */
 @WebServlet(urlPatterns = {"/modifyScalesRouter"})
-public class modifyScalesRouter {
+public class modifyScalesRouter extends HttpServlet {
 
+    //logging class
+    private final Logger logger = LogManager.getLogger(this.getClass());
 
-
-
-
-
+    /**
+     * forwards user to scale management interface
+     * @param request Http object
+     * @param response Http object
+     * @throws ServletException in event of servlet failure
+     * @throws IOException in event of IO failure
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/scalesPage.jsp");
+        dispatcher.forward(request, response);
+        logger.info("modifyScalesRouter executed: {}", LocalDateTime.now().toString());
+    }
 }
