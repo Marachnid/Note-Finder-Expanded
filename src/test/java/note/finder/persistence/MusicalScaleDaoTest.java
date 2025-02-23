7e5619843dd8157a2d331c868c1b1df7cf4df01e
Marachnid <mcherry2@madisoncollege.edu>
2025-02-23 12:36:33 -0600

    All tests passing in MusicalScaleDaoTest, no issues with Hibernate or SQL

--- src/test/java/note/finder/persistence/MusicalScaleDaoTest.java
+++ src/test/java/note/finder/persistence/MusicalScaleDaoTest.java
@@ -1,14 +1,14 @@
 package note.finder.persistence;
 
 import note.finder.entity.MusicalScale;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ */
 class MusicalScaleDaoTest {
 
     MusicalScaleDao scaleDao;
@@ -37,7 +37,18 @@ class MusicalScaleDaoTest {
     @Test
     void update() {
 
+        MusicalScale scale = scaleDao.getById(1);
+        scale.setName("Augmented Minor");
+        scale.setRoot(0);
+        scale.setSecond(1);
+        scale.setThird(2);
+        scaleDao.update(scale);
 
+        MusicalScale updatedScale = scaleDao.getById(1);
+        assertEquals("Augmented Minor", updatedScale.getName());
+        assertEquals(0, updatedScale.getRoot());
+        assertEquals(1, updatedScale.getSecond());
+        assertEquals(2, updatedScale.getThird());
     }
 
     @Test
@@ -58,24 +69,32 @@ class MusicalScaleDaoTest {
     @Test
     void delete() {
 
+        MusicalScale scaleToDelete = scaleDao.getById(1);
+        scaleDao.delete(scaleToDelete);
 
+        List<MusicalScale> updatedScales = scaleDao.getAll();
+        assertEquals(2, updatedScales.size());
+        assertNull(scaleDao.getById(1));
     }
 
     @Test
     void getAll() {
 
-
+        List<MusicalScale> scales = scaleDao.getAll();
+        assertEquals(3, scales.size());
     }
 
     @Test
     void getByPropertyEqual() {
 
-
+        List<MusicalScale> scales = scaleDao.getByPropertyEqual("name", "hungarian minor");
+        assertEquals(1, scales.size());
     }
 
     @Test
     void getByPropertyLike() {
 
-
+        List<MusicalScale> scales = scaleDao.getByPropertyLike("name", "minor");
+        assertEquals(2, scales.size());
     }
 }
