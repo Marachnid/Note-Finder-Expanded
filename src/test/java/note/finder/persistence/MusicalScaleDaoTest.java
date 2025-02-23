06591e1804c9a1b9ca17189ba56b688243a4ae1f
Marachnid <mcherry2@madisoncollege.edu>
2025-02-23 12:07:01 -0600

    Hibernate interactions working now - Updated classes and reconfigured demo resources (not sure what the prior issue, was previously using javax.persistence imports instead of jakarta)

--- src/test/java/note/finder/persistence/MusicalScaleDaoTest.java
+++ src/test/java/note/finder/persistence/MusicalScaleDaoTest.java
@@ -1,15 +1,81 @@
 package note.finder.persistence;
 
 import note.finder.entity.MusicalScale;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
+class MusicalScaleDaoTest {
 
-public class MusicalScaleDaoTest {
+    MusicalScaleDao scaleDao;
+
+    @BeforeEach
+    void setUp() {
+
+        scaleDao = new MusicalScaleDao();
+
+        Database database = Database.getInstance();
+        database.runSQL("short_scales_clean.sql");
+    }
+
+    @Test
+    void getByIdSuccess() {
+
+        MusicalScale scale = scaleDao.getById(1);
+        assertNotNull(scale);
+        assertEquals("Natural Minor", scale.getName());
+        assertEquals(0, scale.getRoot());
+        assertEquals(2, scale.getSecond());
+        assertEquals(3, scale.getThird());
+
+    }
+
+    @Test
+    void update() {
 
 
+    }
+
+    @Test
+    void insert() {
+
+        MusicalScale newUser = new MusicalScale("Test", 1, 2, 3);
+        int newUserId = scaleDao.insert(newUser);
+        assertNotEquals(0, newUserId);
+
+        MusicalScale createdUser = scaleDao.getById(newUserId);
+        assertEquals("Test", createdUser.getName());
+        assertEquals(1, createdUser.getRoot());
+        assertEquals(2, createdUser.getSecond());
+        assertEquals(3, createdUser.getThird());
+
+    }
+
+    @Test
+    void delete() {
+
+
+    }
+
+    @Test
+    void getAll() {
+
+
+    }
+
+    @Test
+    void getByPropertyEqual() {
+
+
+    }
+
+    @Test
+    void getByPropertyLike() {
+
+
+    }
 }
