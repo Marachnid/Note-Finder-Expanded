9e1a177934a471f17a897ff9639fc36d27b76658
Marachnid <mcherry2@madisoncollege.edu>
2025-02-28 21:53:30 -0600

    Updated testing database

---
+++ src/test/java/note/finder/persistence/NoteFinderDaoTest.java
@@ -0,0 +1,46 @@
+package note.finder.persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoteFinderDaoTest {
+
+    NoteFinderDao dao;
+
+
+    @BeforeEach
+    void setUp() {
+
+//        dao = new NoteFinderDao();
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getByPropertyEqual() {
+    }
+
+    @Test
+    void getByPropertyLike() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
--- src/test/resources/category_clean.sql
+++ src/test/resources/category_clean.sql
@@ -16,35 +16,27 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `musical_scale_intervals`
+-- Table structure for table `category`
 --
 
-DROP TABLE IF EXISTS `musical_scale_intervals`;
+DROP TABLE IF EXISTS `category`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `musical_scale_intervals` (
-  `scale_id` int NOT NULL AUTO_INCREMENT,
-  `scale_name` varchar(50) NOT NULL,
-  `root_interval` tinyint unsigned NOT NULL,
-  `second_interval` tinyint unsigned NOT NULL,
-  `third_interval` tinyint unsigned NOT NULL,
-  `fourth_interval` tinyint unsigned NOT NULL,
-  `fifth_interval` tinyint unsigned NOT NULL,
-  `sixth_interval` tinyint unsigned NOT NULL,
-  `seventh_interval` tinyint unsigned NOT NULL,
-  PRIMARY KEY (`scale_id`),
-  UNIQUE KEY `unique_name` (`scale_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `category` (
+  `category_id` int NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(30) NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `musical_scale_intervals`
+-- Dumping data for table `category`
 --
 
-LOCK TABLES `musical_scale_intervals` WRITE;
-/*!40000 ALTER TABLE `musical_scale_intervals` DISABLE KEYS */;
-INSERT INTO `musical_scale_intervals` (`scale_id`, `scale_name`, `root_interval`, `second_interval`, `third_interval`, `fourth_interval`, `fifth_interval`, `sixth_interval`, `seventh_interval`) VALUES (1,'Natural Minor',0,2,3,5,7,8,10),(4,'Natural Major',0,2,4,5,7,9,11),(5,'Hungarian Minor',0,2,3,6,7,8,11);
-/*!40000 ALTER TABLE `musical_scale_intervals` ENABLE KEYS */;
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` (`category_id`, `category_name`) VALUES (1,'Major'),(2,'Minor'),(3,'Diminished'),(4,'Augmented');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -56,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-02-22 20:34:06
+-- Dump completed on 2025-02-28 17:02:50
