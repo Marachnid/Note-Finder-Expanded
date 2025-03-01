0ecf60ead3ab18a253055e8d1084f3cd9d464b5c
Marachnid <mcherry2@madisoncollege.edu>
2025-02-28 19:07:52 -0600

    Added getters and setters for new classes

--- src/main/java/note/finder/entity/MusicalCategory.java
+++ src/main/java/note/finder/entity/MusicalCategory.java
@@ -16,6 +16,40 @@ public class MusicalCategory {
     @Column(name = "category_name")
     private String categoryName;
 
-    @OneToMany(mappedBy = "musicalCategory", cascade = CascadeType.ALL, orphanRemoval = false)
+    @OneToMany(mappedBy = "musicalCategory", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<MusicalScale> musicalScales;
+
+
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Set<MusicalScale> getMusicalScales() {
+        return musicalScales;
+    }
+
+    public void setMusicalScales(Set<MusicalScale> musicalScales) {
+        this.musicalScales = musicalScales;
+    }
+
+
+    public String toString() {
+
+        return "";
+    }
 }
