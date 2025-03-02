66add95f7a92c5142be31e74e42fc7a1941c2c01
Marachnid <mcherry2@madisoncollege.edu>
2025-03-02 17:57:27 -0600

    Removed unused imports - updated javadoc

--- src/main/java/note/finder/persistence/NoteFinderDao.java
+++ src/main/java/note/finder/persistence/NoteFinderDao.java
@@ -1,25 +1,22 @@
 package note.finder.persistence;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.TypedQuery;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Expression;
 import jakarta.persistence.criteria.Root;
-import note.finder.entity.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.criteria.HibernateCriteriaBuilder;
-
 import java.util.List;
 
+/**
+ * Generic DAO class for executing queries of different class types
+ * @param <T> class types being passed into DAO
+ */
 public class NoteFinderDao<T> {
 
     private final Class<T> type;
-    private EntityManager entityManager;
     private final Logger logger = LogManager.getLogger(this.getClass());
 
     /**
