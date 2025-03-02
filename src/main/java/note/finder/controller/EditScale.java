f3e0090a7bb1afb484c1dc149eb7146354513415
Marachnid <mcherry2@madisoncollege.edu>
2025-03-02 16:16:53 -0600

    Commented out method calls - logic needs to be reworked after DAO changes

--- src/main/java/note/finder/controller/EditScale.java
+++ src/main/java/note/finder/controller/EditScale.java
@@ -15,28 +15,28 @@ import java.io.IOException;
 @WebServlet(urlPatterns = "/updateScale")
 public class EditScale extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//
+//        MusicalScaleDao scaleDao = new MusicalScaleDao();
+//        MusicalScale scaleToEdit = returnUpdatedScaleObject(request);
+//
+//        scaleDao.update(scaleToEdit);
+//
+//        request.removeAttribute("scaleToEdit");
+//        response.sendRedirect(request.getContextPath() + "/modifyScalesRouter");
+//
+//    }
 
-        MusicalScaleDao scaleDao = new MusicalScaleDao();
-        MusicalScale scaleToEdit = returnUpdatedScaleObject(request);
 
-        scaleDao.update(scaleToEdit);
-
-        request.removeAttribute("scaleToEdit");
-        response.sendRedirect(request.getContextPath() + "/modifyScalesRouter");
-
-    }
-
-
-    private MusicalScale returnUpdatedScaleObject(HttpServletRequest request) {
-
-        String name = request.getParameter("name");
-        int id = Integer.parseInt(request.getParameter("id"));
-        int root = Integer.parseInt(request.getParameter("root"));
-        int second = Integer.parseInt(request.getParameter("second"));
-        int third = Integer.parseInt(request.getParameter("third"));
-
-        return new MusicalScale(name, id, root, second, third);
-    }
+//    private MusicalScale returnUpdatedScaleObject(HttpServletRequest request) {
+//
+//        String name = request.getParameter("name");
+//        int id = Integer.parseInt(request.getParameter("id"));
+//        int root = Integer.parseInt(request.getParameter("root"));
+//        int second = Integer.parseInt(request.getParameter("second"));
+//        int third = Integer.parseInt(request.getParameter("third"));
+//
+//        return new MusicalScale(name, id, root, second, third);
+//    }
 }
